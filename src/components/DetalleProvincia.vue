102e3a09d6fa631f47976a36b23a14a7f225ffb8
Juan Vinci <jm_vincii@hotmail.com>
2022-06-15 19:02:37 -0300

    .

--- src/components/DetalleProvincia.vue
+++ src/components/DetalleProvincia.vue
@@ -1,27 +1,29 @@
 <template lang="">
-  <div class="w-50 carta p-3 rounded mt-5">
-    <div class="row">
-      <div class="col">
-        <p>Provincia:</p>
-        <p>Abreviatura:</p>
-        <p>Capital:</p>
-        <p>Población:</p>
-        <p>Superficie: Nº</p>
-        <p>Departamentos:</p>
-        <p>Bandera:</p>
+  <div class="centrar">
+    <div class="w-50 carta p-3 rounded mt-5">
+      <div class="row">
+        <div class="col">
+          <p>Provincia:</p>
+          <p>Abreviatura:</p>
+          <p>Capital:</p>
+          <p>Población:</p>
+          <p>Superficie: Nº</p>
+          <p>Departamentos:</p>
+          <p>Bandera:</p>
+        </div>
+        <div class="col">
+          <p>{{ provincia.nombre }}</p>
+          <p>{{ provincia.abreviatura }}</p>
+          <p>{{ provincia.capital }}</p>
+          <p v-if="provincia.poblacion">{{ provincia.poblacion }}</p>
+          <p v-else>No asigna</p>
+          <p>{{ provincia.superficie }}</p>
+          <p>{{ provincia.nroDepartamentos }}</p>
+          <p><img :src="'/img/' + provincia.bandera" /></p>
+        </div>
       </div>
-      <div class="col">
-        <p>{{ provincia.nombre }}</p>
-        <p>{{ provincia.abreviatura }}</p>
-        <p>{{ provincia.capital }}</p>
-        <p v-if=" provincia.poblacion">{{ provincia.poblacion }}</p>
-        <p v-else>No asigna</p>
-        <p>{{ provincia.superficie }}</p>
-        <p>{{ provincia.nroDepartamentos }}</p>
-        <p><img :src="'/img/' + provincia.bandera" /></p>
-      </div>
+      <a class="btn btn-primary" @click="$router.go(-1)">Volver</a>
     </div>
-    <a class="btn btn-primary" @click="$router.go(-1)">Volver</a>
   </div>
 </template>
 <script>
@@ -44,13 +46,16 @@ export default {
       );
       _provincia = await _provincia.json();
       this.provincia = _provincia;
-  
     },
   },
 };
 </script>
 <style scoped>
-.carta{
-    border: solid 2px grey;
+.carta {
+  border: solid 2px grey;
+}
+.centrar {
+  display: flex;
+  justify-content: center;
 }
 </style>
